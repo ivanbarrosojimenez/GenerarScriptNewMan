c85af0b23d10fc5972aef2394e215ad44f8143b4
ibarrosoj <ibarrosj@everis.com>
2021-02-25 10:00:46 +0100

    Cambio para generar colecciones en dos entornos

--- src/bat/GenerarScriptNewMan.java
+++ src/bat/GenerarScriptNewMan.java
@@ -6,47 +6,58 @@ import java.io.IOException;
 public class GenerarScriptNewMan {
 
     public static void main(String[] args) throws IOException {
-
+    	/** Modificar solo esto*/
         int fase = 1;
         int numPruebas = 18;
+        /** Modificar solo esto*/
+        
+        
         String entornoDB2 = "eDb2Estable.json";
-        String entornoSQL = "eSQL.json";
-        String entornoComparacion = entornoDB2;
+        String entornoSQL = "eSQLEstable.json";
+        String [] entornoComparacion = new String[2];
+        entornoComparacion[0] = entornoDB2;
+        entornoComparacion[1] = entornoSQL;
 
-        StringBuffer sfRespuesta = new StringBuffer();
+        for (int entorno = 0; entorno < 2; entorno++) {
+        	 StringBuffer sfRespuesta = new StringBuffer();
+             String carpetaResultados = "resultados"+entornoComparacion[entorno].substring(1, 4);
+             String carpetaGlobales = "globales"+entornoComparacion[entorno].substring(1, 4);
 
-        for (int prueba = 0; prueba < numPruebas; prueba++) {
-            sfRespuesta = new StringBuffer();
-            sfRespuesta.append("mkdir resultados\n");
-            sfRespuesta.append("mkdir globales\n");
-            String l1 = "call newman run Colecciones\\ColeccionPostman_fase_" + fase + "_" + prueba
-                    + ".json -e eUrsaePru.json --reporters cli,json --reporter-json-export globales\\globales"
-                    + prueba + ".json\n";
-            sfRespuesta.append(l1);
-            String l2 = "call newman run Colecciones\\ColeccionPostman_fase_" + fase + "_" + prueba
-                    + ".json -e " + entornoComparacion + " -g globales\\globales" + prueba
-                    + ".json -d globales\\globales" + prueba
-                    + ".json --reporters cli,json --reporter-json-export resultados\\respuesta_"
-                    + prueba + ".json\n";
-            sfRespuesta.append(l2);
+             for (int prueba = 0; prueba < numPruebas; prueba++) {
+                 sfRespuesta = new StringBuffer();
+                 sfRespuesta.append("mkdir "+carpetaResultados+"\n");
+                 sfRespuesta.append("mkdir "+carpetaGlobales+"\n");
+                 String l1 = "call newman run ..\\Colecciones\\ColeccionPostman_fase_" + fase + "_" + prueba
+                         + ".json -e ..\\eUrsaePru.json --reporters cli,json --reporter-json-export "+carpetaGlobales+"\\"
+                         + prueba + ".json\n";
+                 sfRespuesta.append(l1);
+                 String l2 = "call newman run ..\\Colecciones\\ColeccionPostman_fase_" + fase + "_" + prueba
+                         + ".json -e ..\\" + entornoComparacion[0] + " -g "+carpetaGlobales+"\\" + prueba
+                         + ".json -d "+carpetaGlobales+"\\" + prueba
+                         + ".json --reporters cli,json --reporter-json-export "+carpetaResultados+"\\respuesta_"
+                         + prueba + ".json\n";
+                 sfRespuesta.append(l2);
 
-            sfRespuesta.append("echo * * * * FIN DEL PROCESO. * * * * \n");
-            // sfRespuesta.append(":end\n");
-            // sfRespuesta.append("echo \n");
-            // sfRespuesta
-            // .append("echo * * * * * * * * * * * * * FIN DEL PROCESO!! * * * * * * * \n");
-            // sfRespuesta.append("timeout /t 1\n");
-            // sfRespuesta.append("cls\n");
-            // sfRespuesta.append("goto end\n");
-            // sfRespuesta.append("pause\n");
+                 sfRespuesta.append("echo * * * * FIN DEL PROCESO. * * * * \n");
+                 // sfRespuesta.append(":end\n");
+                 // sfRespuesta.append("echo \n");
+                 // sfRespuesta
+                 // .append("echo * * * * * * * * * * * * * FIN DEL PROCESO!! * * * * * * * \n");
+                 // sfRespuesta.append("timeout /t 1\n");
+                 // sfRespuesta.append("cls\n");
+                 // sfRespuesta.append("goto end\n");
+                  sfRespuesta.append("pause\n");
 
-            GrabarFichero grabarFichero = new GrabarFichero();
-            grabarFichero.crearFichero("scriptPruebasFase" + fase + "_p_" + prueba + ".bat", false);
+                 GrabarFichero grabarFichero = new GrabarFichero();
+                 grabarFichero.crearFichero("ScriptFase"+fase+"_"+entornoComparacion[entorno].substring(0, 11)+"/scriptPruebasFase" + fase + "_p_" + prueba + ".bat", true);
 
-            grabarFichero.agregarAFichero(sfRespuesta.toString());
-            grabarFichero.cerrarFichero();
-            System.out.println("Fin de generacion bat");
-        }
+                 grabarFichero.agregarAFichero(sfRespuesta.toString());
+                 grabarFichero.cerrarFichero();
+                 System.out.println("Fin de generacion bat " +entornoComparacion[entorno].substring(1, 4) + " fase " + fase);
+             }
+		}
+
+       
     }
 
 }
