a1607514c9bb549f213c69f4baacc775e0b97cc3
Carlos Benavent Olmo <carlos.benavent@inetum.world>
2021-05-21 13:28:10 +0200

    Se aumenta array para mayor numero de pruebas.

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import bat.GenerarScriptNewMan;
 
 public class Main {
-	private static String [] colores = {"1f","26","37","48","59","60","7a","8b","9c","ad","be","cf","d1","e2","af","c9","9c","1f","26","37","48","59","60","7a","8b","9c","ad","be","cf","d1","e2","af","c9","9c","1f","26","37","48","59","60","7a","8b","9c","ad","be","cf","d1","e2","af","c9","9c","1f","26","37","48","59","60","7a","8b","9c","ad","be","cf","d1","e2","af","c9","9c"};
+	private static String [] colores = {"1f","26","37","48","59","60","7a","8b","9c","ad","be","cf","d1","e2","af","c9","9c","1f","26","37","48","59","60","7a","8b","9c","ad","be","cf","d1","e2","af","c9","9c","1f","26","37","48","59","60","7a","8b","9c","ad","be","cf","d1","e2","af","c9","9c","1f","26","37","48","59","60","7a","8b","9c","ad","be","cf","d1","e2","af","c9","9c","1f","26","37","48","59","60","7a","8b","9c","ad","be","cf","d1","e2","af","c9","9c","1f","26","37","48","59","60","7a","8b","9c","ad","be","cf","d1","e2","af","c9","9c","1f","26","37","48","59","60","7a","8b","9c","ad","be","cf","d1","e2","af","c9","9c"};
     private static String filtro = "";
     static String [] entornoComparacion = obtenerNombresEntornos(); 
     
@@ -17,16 +17,16 @@ public class Main {
         
     	/** Modificar solo esto para resultados*/
 		boolean generarBatUnico = true;
-        /*int fase = 10;
-        int numPruebas = 68;
+        int fase = 9;
+        int numPruebas = 51;
         String version = "v10";
         filtro = "*";//Poner NoEstable para generar las 2 pruebas estables, * para todas
         /** Modificar solo esto*/
         
-        //resultados.generarScriptResultado(fase, numPruebas, version, generarBatUnico, version, entornoComparacion);
+        resultados.generarScriptResultado(fase, numPruebas, version, generarBatUnico, version, entornoComparacion);
         
     	/** Modificar solo esto para tiempos*/
-        int numIteraciones = 3;
+        /*int numIteraciones = 3;
 		generarBatUnico = true;
         int fase = 9;
         int numPruebas = 39;
@@ -35,7 +35,7 @@ public class Main {
         filtro = "*";//Poner NoEstable para generar las 2 pruebas estables, * para todas*/
         /** Modificar solo esto para tiempos*/
         
-        resultados.generarScriptTiempos(compararPre, numIteraciones, fase, numPruebas, version, generarBatUnico, version, entornoComparacion);
+        //resultados.generarScriptTiempos(compararPre, numIteraciones, fase, numPruebas, version, generarBatUnico, version, entornoComparacion);
         //resultados.generarScriptTiempos(numIteraciones, fase, numPruebas, version, generarBatUnico, version, "esSqlEstable.json");
 
 	}
